58232b1d50dd6161cf5cda0ccff5e492cb538ed4
hritick <hritick.mallick@innoraft.com>
2023-03-24 12:35:21 +0530

    implemented the counter-up plugin

--- JS/index.js
+++ JS/index.js
@@ -148,3 +148,35 @@ $(document).ready(function(){
         $('.hamburger-nav-close-btn').css('display','none')
     })
 })
+
+
+const counterUp = window.counterUp.default
+
+const callback = entries => {
+	entries.forEach( entry => {
+		const el = entry.target
+		if ( entry.isIntersecting && ! el.classList.contains( 'is-visible' ) ) {
+                    for(const counter of counters) {
+      	              counterUp( counter, {
+      		              duration: 3000,
+      		              delay: 16,
+      	              })
+                    el.classList.add( 'is-visible' )
+                  }
+	    }
+	} )
+}
+
+// observer
+const IO = new IntersectionObserver( callback, { threshold: 1 } )
+
+// First element to target
+const el = document.querySelector( '.counter' )
+
+// all numbers
+const counters = document.querySelectorAll( '.counter' )
+IO.observe( el )
+
+
+
+
